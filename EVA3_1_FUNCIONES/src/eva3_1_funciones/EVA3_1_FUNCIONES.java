705c8a3c1606617f84dc43194258a64a73d3bb20
faze-useless <143573781+faze-useless@users.noreply.github.com>
2023-11-22 19:03:57 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,20 @@
+
+package eva3_1_funciones;
+import java.util.*;
+
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //llamar a la funcion
+        String a = "Hola Mundo!";
+        print(a);
+        
+        print("Hello World!");
+    }
+    
+    public static void print(String message) {
+        System.out.println(message);
+    }
+    
+}
